d1ab83bda238757489f316083f3169f8d6c2050a
Ilya Chesalin <evilyach@protonmail.com>
2020-01-26 14:04:08 +0000

    Added min() function solutions

---
+++ chapter-3/1-min.js
@@ -0,0 +1,39 @@
+// Solution #1
+// the simplest
+const min = (a, b) => {return a < b ? a : b};
+
+console.log(min(1, 3));
+
+// Solution #2
+// PrOdUcTiOn MeMe
+class FancyCompanyNameGetMin {
+    getMin(a, b) {
+        try {
+            if (isNaN(a)) {
+                console.log(`[!] ERROR: First parameter a=${a} is not a number!`);
+                return undefined;
+            }
+    
+            if (isNaN(b)) {
+                console.log(`[!] ERROR: Second parameter b=${b} is not a number!`);
+                return undefined;
+            }
+    
+            try {
+                if (a < b) {
+                    return a;
+                } else if (a > b) {
+                    return b;
+                } else {
+                    console.log(`[!] ERROR: Could not find min out of a = ${a} and b = ${b}`)
+                }
+            } catch (e) {
+                console.log(`[!] ERROR: Could not find min out of a = ${a} and b = ${b}: ${e}`)
+            }
+        } catch (e) {
+            console.log(`[!] ERROR: Could not find min out of a = ${a} and b = ${b}: ${e}`)
+        }
+    }
+}
+
+console.log(new FancyCompanyNameGetMin().getMin(1, 3));
